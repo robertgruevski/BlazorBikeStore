0de2417b0b9e05dbebdd1740f81539f659cd9965
robertgruevski <robertgruevski@outlook.com>
2025-03-10 16:37:19 -0400

    Scaffolded creation for bike pages

---
+++ Data/BlazorBikeStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorBikeStore.Models;
+
+namespace BlazorBikeStore.Data
+{
+    public class BlazorBikeStoreContext : DbContext
+    {
+        public BlazorBikeStoreContext (DbContextOptions<BlazorBikeStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorBikeStore.Models.Bike> Bike { get; set; } = default!;
+    }
+}
